6a67cf246dde39f3f1cd6d708f74e4910a35f927
Kiserexser <towartors@mail.ru>
2026-06-27 12:32:17 +0300

    Update DusaruysClient.java

--- src/main/java/name/modid/DusaruysClient.java
+++ src/main/java/name/modid/DusaruysClient.java
@@ -1,33 +1,14 @@
-package name.modid.client;
+package name.modid;
 
-import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
-import net.minecraft.client.KeyMapping;
-import net.minecraft.client.gui.GuiGraphics;
-import org.lwjgl.glfw.GLFW;
-import name.modid.Arrows;
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class DusaruysClientClient implements ClientModInitializer {
+public class DusaruysClient implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("dusaruysclient");
+
     @Override
-    public void onInitializeClient() {
-        KeyMapping keyBinding = KeyBindingHelper.registerKeyBinding(new KeyMapping(
-                "key.arrows.toggle",
-                GLFW.GLFW_KEY_Z,
-                "category.arrows"
-        ));
-
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (keyBinding.consumeClick()) {
-                Arrows.toggle();
-            }
-        });
-
-        HudRenderCallback.EVENT.register((context, tickDelta) -> {
-            if (Arrows.isEnabled()) {
-                Arrows.render((GuiGraphics) context);
-            }
-        });
+    public void onInitialize() {
+        LOGGER.info("DusaruysClient loaded!");
     }
 }
